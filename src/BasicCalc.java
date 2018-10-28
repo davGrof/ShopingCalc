ab0dfa40d58c536542ec0228a94a07ec763cb141
David <grofdav@gmail.com>
2018-10-28 11:56:05 +0200

    second

--- src/BasicCalc.java
+++ src/BasicCalc.java
@@ -1,6 +1,6 @@
 public class BasicCalc {
     public static void main(String[] args) {
         System.out.println("Calcolate");
-        System.out.println("hello");
+        System.out.println("Hello");
     }
 }
